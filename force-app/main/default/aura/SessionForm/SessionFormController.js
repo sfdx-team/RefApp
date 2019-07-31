91f82071f7df15ef400af20e062bd8fe0691b1bf
Gregorio <vasques36@gmail.com>
2019-07-31 19:26:24 -0400

    add sessionformedit

--- force-app/main/default/aura/SessionForm/SessionFormController.js
+++ force-app/main/default/aura/SessionForm/SessionFormController.js
@@ -1,5 +1,18 @@
 ({
-    myAction : function(component, event, helper) {
+    onSuccess : function(component, event, helper) {
+        //show succes message on upsertion of record
+        var resultToast = $A.get("e.force:showToast");
+        resultToast.setParams({
+            "title" : "Success",
+            "message" : "Session Record Saved Successfully"
+        });
+        resultToast.fire();
+        //navigate to object homepage
+        var homeEvent = $A.get("e.force:navigateToObjectHome");
+        homeEvent.setParams({
+            "scope" : "Session__c"
+        });
+        homeEvent.fire();
 
     }
 })
